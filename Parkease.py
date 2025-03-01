dd44548cd0f44b5724f22886b57fe558e24a7d65
Artilox08 <matt.chapman76113@gmail.com>
2025-03-01 11:54:12 +0100

    entrée/sortie faite

--- Parkease.py
+++ Parkease.py
@@ -1,13 +1,21 @@
 from parking import *
 from database import *
 from config import *
+from datetime import datetime
 
 data = lecture_json()
 place_dispo = len(data.get("voitures"))
 
-plaque = input("Veuillez entrer votre plaque d'immatriculation: ")
+plaque = str(input("Veuillez entrer votre plaque d'immatriculation: "))
 
 voitures = data.get("voitures", [])
 plaque_existe = any(voiture.get("plaque") == plaque for voiture in voitures)
 
-# if plaque_existe != True:
+heure = datetime.now()
+heure = heure.strftime("%H:%M:%S")
+
+if plaque_existe == False:
+    enter(plaque,heure)
+
+elif plaque_existe == True:
+    exit(plaque)
