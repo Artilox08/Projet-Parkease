4130a7420cfeeedb34be630ee80439f090b7f5ed
Artilox08 <matt.chapman76113@gmail.com>
2025-03-01 11:04:26 +0100

    commit

--- Parkease.py
+++ Parkease.py
@@ -5,3 +5,9 @@ from config import *
 data = lecture_json()
 place_dispo = len(data.get("voitures"))
 
+plaque = input("Veuillez entrer votre plaque d'immatriculation: ")
+
+voitures = data.get("voitures", [])
+plaque_existe = any(voiture.get("plaque") == plaque for voiture in voitures)
+
+# if plaque_existe != True:
